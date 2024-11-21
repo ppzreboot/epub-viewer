ac21a9a94e96e0195cf8f7dc66e45fe4db3c0cc2
PPz <ppzreboot@qq.com>
2024-11-21 16:34:54 +0800

    feat(epub-utils): path utils

---
+++ lib/epub-utils/parse/path.ts
@@ -0,0 +1,16 @@
+export
+const join_path = (base: string, appender: string) => {
+  if (appender.startsWith('/')) // absolute path
+    return appender
+
+  let back_num = 0
+  while(appender.startsWith('../')) {
+    back_num ++
+    appender = appender.slice(3)
+  }
+
+  let bases = base.split('/')
+  if (back_num)
+    bases = bases.slice(0, -back_num)
+  return [...bases, ...appender.split('/')].join('/')
+}

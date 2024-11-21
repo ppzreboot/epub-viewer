5abcbb19fd9ed84b09e4a83d4486c0225675a017
PPz <ppzreboot@qq.com>
2024-11-21 14:31:19 +0800

    feat(epub-utils): parse manifest

--- lib/epub-utils/parse/meta.ts
+++ lib/epub-utils/parse/meta.ts
@@ -1,11 +1,12 @@
 import { parse_xml, xml_attr } from '../utils'
-import { I_epub_files, I_epub_meta } from '../types'
+import { I_epub_files, I_epub_meta, I_manifest } from '../types'
 
 export
 const parse_meta = (epub: I_epub_files): I_epub_meta => {
   const container = parse_xml(epub['META-INF/container.xml'])
   const rootfile_path = xml_attr(container.querySelector('rootfile')!, 'full-path')
   const rootfile = parse_xml(epub[rootfile_path])
+
   const basic = rootfile.querySelector('metadata')!
   return {
     title: basic.querySelector('title')?.innerHTML,
@@ -16,5 +17,12 @@ const parse_meta = (epub: I_epub_files): I_epub_meta => {
       if (date)
         return new Date(date)
     })(),
+    manifest: parse_manifest(rootfile.querySelector('manifest')!),
   }
 }
+
+const parse_manifest = (el: Element): I_manifest[] =>
+  Array.from(el.children).map(item => ({
+    id: xml_attr(item, 'id'),
+    href: xml_attr(item, 'href'),
+  }))
--- lib/epub-utils/types.ts
+++ lib/epub-utils/types.ts
@@ -1,3 +1,10 @@
+export
+interface I_manifest {
+  id: string
+  href: string
+  // media_type: string
+}
+
 /** information defined in rootfile of epub */
 export
 interface I_epub_meta {
@@ -5,6 +12,7 @@ interface I_epub_meta {
   creator?: string
   publisher?: string
   date?: Date
+  manifest: I_manifest[]
 }
 
 export
